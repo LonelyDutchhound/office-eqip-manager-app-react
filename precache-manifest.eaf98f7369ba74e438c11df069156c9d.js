6d7b524fc65b3ea33b65ff802d6ea51720aad163
Elena <lonely.dutchhound@gmail.com>
2019-11-11 18:01:30 +0300

    Updates

---
+++ precache-manifest.eaf98f7369ba74e438c11df069156c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54f3b84d8bb6fbfad071954cf44ed11",
+    "url": "/office-equip-manager-app/index.html"
+  },
+  {
+    "revision": "f1c4572f4f713107b9aa",
+    "url": "/office-equip-manager-app/static/css/main.083c2793.chunk.css"
+  },
+  {
+    "revision": "40aa8e8a3a1935952be9",
+    "url": "/office-equip-manager-app/static/js/2.14500fd3.chunk.js"
+  },
+  {
+    "revision": "f1c4572f4f713107b9aa",
+    "url": "/office-equip-manager-app/static/js/main.d05bbeb7.chunk.js"
+  },
+  {
+    "revision": "f476cc9bf0740fccfded",
+    "url": "/office-equip-manager-app/static/js/runtime-main.fb01168d.js"
+  }
+]);
