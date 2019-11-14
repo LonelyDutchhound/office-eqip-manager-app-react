e3798dc05d653243a241f0b927ccb260ea638261
Elena <lonely.dutchhound@gmail.com>
2019-11-14 11:17:52 +0300

    Updates

---
+++ precache-manifest.7c7dc12107ab47bb5e9bcdcfc267c532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d68e5350d6d65ded717df12236a423c",
+    "url": "/office-eqip-manager-app-react/index.html"
+  },
+  {
+    "revision": "41545c70d471e34e1e9d",
+    "url": "/office-eqip-manager-app-react/static/css/main.22e98960.chunk.css"
+  },
+  {
+    "revision": "7fe3e152a0624443e4f3",
+    "url": "/office-eqip-manager-app-react/static/js/2.b967fb65.chunk.js"
+  },
+  {
+    "revision": "41545c70d471e34e1e9d",
+    "url": "/office-eqip-manager-app-react/static/js/main.35e927c8.chunk.js"
+  },
+  {
+    "revision": "68e393704b122becc2c2",
+    "url": "/office-eqip-manager-app-react/static/js/runtime-main.6dad526f.js"
+  }
+]);
